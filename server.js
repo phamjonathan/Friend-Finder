badf2284a073fbf4e70287016418db70b5e08e26
Jonathan Pham <jonathanthienpham@gmail.com>
2020-02-29 10:59:28 -0500

    friend finder

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require("express")
+var app = express()
+
+var PORT = process.env.PORT || 8080
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+
+app.use(express.static("public"))
+
+var apiRoutes = require("./app/routing/apiRoutes")
+var htmlRoutes = require("./app/routing/htmlRoutes")
+
+apiRoutes(app)
+htmlRoutes(app)
+
+app.listen(PORT,function(){
+    console.log("app is listening http://localhost:" + PORT )
+})
